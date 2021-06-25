5dfbcdd07e8a7da599c9d954b4dd1ba731f21e77
Jordan Mateen <jordan.mateen@coxautoinc.com>
2021-06-24 21:39:45 -0400

    adding socket io

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 //imports
 const IOTA = require('@iota/core');
 const Converter = require('@iota/converter');
+const express = require('express')
+const socket = require('socket.io')
+
+const io_client = require('socket.io-client')
+io_client('http://localhost:5645/');
+
+const app = express() 
+const PORT = 5645;
+
+app.use(express.static('public'))
+
+const server = app.listen(PORT, ()=>{
+    console.log(`Application connected on port ${PORT}....`)
+})
+
+// //setup socket
+const io = socket(server)
 
 // Create an instance of the IOTA API (Node on the devnet)
 const iota = IOTA.composeAPI({
@@ -22,42 +39,49 @@ const seed = process.env.SEED
 const message = JSON.stringify({"message": "Hello world"});
 const messageInTrytes = Converter.asciiToTrytes(message);
 
-//get address
-iota.getNewAddress(seed, options, (error, newAddress)=>{
-   try {
-       const transfers = [
-           {
-               value: 0,
-               address: newAddress,
-               message:messageInTrytes
-           }
-       ]
-       /**
-        * 
-        * @param {*} seed 
-        * @param {*} transfers 
-        * 
-        * functions: 
-        *  prepareTransfers => preparing transfer by doing tip selections. (Where the node will be placed on the graph) 
-        *  sendTrytes => handles the remote proof or work (Remote PoW: When a node does proof of work on your transaction)
-        *  sendTrytes =>  also handles sending the transaction to the node so it can be attached to Tanlge 
-        */
-       
-        iota.prepareTransfers(seed, transfers)
-            .then(trytes => {
-                return iota.sendTrytes(trytes, depth, minimumWeightMagnitude);  
-            })
-            .then(bundle => {
-                console.log(bundle)
-            })
-            .catch(err => {
-                console.error(err)
-            });
 
-   } catch (e) {
-       console.log(error)
-       console.og('There was an error retrieving address')
-   }
+// //execute when socket is connected 
+
+io.on('connection', (socket)=> {
+    console.log('Attached to socket...',socket.id)
+    socket.send('Hello')
+    //get address
+    iota.getNewAddress(seed, options, (error, newAddress)=>{
+       try {
+           const transfers = [
+               {
+                   value: 0,
+                   address: newAddress,
+                   message:messageInTrytes
+               }
+           ]
+           /**
+            * 
+            * @param {*} seed 
+            * @param {*} transfers 
+            * 
+            * functions: 
+            *  prepareTransfers => preparing transfer by doing tip selections. (Where the node will be placed on the graph) 
+            *  sendTrytes => handles the remote proof or work (Remote PoW: When a node does proof of work on your transaction)
+            *  sendTrytes =>  also handles sending the transaction to the node so it can be attached to Tanlge 
+            */
+           
+            iota.prepareTransfers(seed, transfers)
+                .then(trytes => {
+                    return iota.sendTrytes(trytes, depth, minimumWeightMagnitude);  
+                })
+                .then(bundle => {
+                    console.log(bundle)
+                })
+                .catch(err => {
+                    console.error(err)
+                });
+    
+       } catch (e) {
+           console.log(error)
+           console.og('There was an error retrieving address')
+       }
+    })
 })
 
 
